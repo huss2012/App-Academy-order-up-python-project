ab88808549632e92b98c3208b9280da513323b83
huss2012 <huss24940@gmail.com>
2024-11-25 01:01:40 +0300

    Implementing login&logout functionality.

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,20 @@
 from flask import Flask
 from .config import Configuration
-from .routes import orders
-from .models import db
+from .routes import orders, session
+from .models import db, Employee
+from flask_login import LoginManager
 
 app = Flask(__name__)
 app.config.from_object(Configuration)
 app.register_blueprint(orders.bp)
+app.register_blueprint(session.bp)
 db.init_app(app)
+
+#create an instance of the LoginManager
+login = LoginManager(app)
+#instruct the login manager to use seesion.login Blueprint function
+login.login_view = "session.login"
+
+@login.user_loader
+def load_user(id):
+    return Employee.query.get((int(id)))

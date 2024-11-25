253e1b80dc36d561e4824a22a51da391bab512e3
huss2012 <huss24940@gmail.com>
2024-11-25 14:29:33 +0300

    spoting error in app/templates/login.html which is csrf_token.

--- app/routes/session.py
+++ app/routes/session.py
@@ -12,9 +12,11 @@ def login():
         return redirect(url_for("orders.index"))
     form = LoginForm()
     if form.validate_on_submit():
+        print("form submitted successfully")
         empl_number = form.employee_number.data
         employee = Employee.query.filter(Employee.employee_number == empl_number).first()
         if not employee or not employee.check_password(form.password.data):
+            print("You are not employee")
             return redirect(url_for(".login"))
         login_user(employee)
         return redirect(url_for("orders.index"))

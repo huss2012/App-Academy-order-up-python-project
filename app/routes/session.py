47dc349350691e1d4f86f40dc9bb4eda31f10757
huss2012 <huss24940@gmail.com>
2024-11-25 13:59:39 +0300

    testing everything up to this point.

--- app/routes/session.py
+++ app/routes/session.py
@@ -6,12 +6,11 @@ from flask_login import current_user, login_user, logout_user
 bp = Blueprint("session", __name__, url_prefix="/session")
 
 
-@bp.route('/', methods=["GET", "POST"])
+@bp.route("/", methods=["GET", "POST"])
 def login():
     if current_user.is_authenticated:
         return redirect(url_for("orders.index"))
     form = LoginForm()
-
     if form.validate_on_submit():
         empl_number = form.employee_number.data
         employee = Employee.query.filter(Employee.employee_number == empl_number).first()
@@ -20,6 +19,7 @@ def login():
         login_user(employee)
         return redirect(url_for("orders.index"))
     return render_template("login.html", form=form)
+
 @bp.route('/logout', methods=["POST"])
 def logout():
     logout_user()

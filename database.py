6c17ebe412b868f2f8f1de4f1e2e19321741577c
huss2012 <huss24940@gmail.com>
2024-11-26 18:55:24 +0300

    define the order and orderdetails class model.

--- database.py
+++ database.py
@@ -6,7 +6,7 @@ load_dotenv()
 # so that the environment variables are
 # properly loaded.
 from app import app, db
-from app.models import Employee, Menu, MenuItem, MenuItemType
+from app.models import Employee, Menu, MenuItem, MenuItemType, Table
 
 
 with app.app_context():
@@ -14,15 +14,24 @@ with app.app_context():
     db.create_all()
 
     employee = Employee(name="Margot", employee_number=1234, password="password")
+    db.session.add(employee)
+
     beverages  = MenuItemType(name="Beverages")
     entrees = MenuItemType(name="Entrees")
     sides = MenuItemType(name="Sides")
     dinner = Menu(name="Dinner")
-
     fries = MenuItem(name="French fries", price=3.5, type=sides, menu=dinner)
     drp = MenuItem(name="Dr. Pepper", price=1.0, type=beverages, menu=dinner)
     jambalaya = MenuItem(name="Jambalaya", price=21.98, type=entrees, menu=dinner)
-
     db.session.add(dinner) # Add one and SQLAlchemy will handel the adding the rest due to the relationship defined in the model class definetion
-    db.session.add(employee)
+
+
+    table_data = [(i, i*2) for i in range(1, 11)]
+    table_object = [Table(number=tableNum, capacity=tableCapacity) for tableNum, tableCapacity in table_data]
+    db.session.add_all(table_object)
+
+
+
+
+
     db.session.commit()
